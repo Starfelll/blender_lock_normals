45530b31627b28bd0a200ae4e9594eb56dca3746
Starfelll <daluobeizi@gmail.com>
2025-07-27 15:33:50 +0800

    1.1

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from mathutils import Vector
 bl_info = {
     "name": "Lock Normals",
     "blender": (4, 5, 0),
-    "version": (1, 0),
+    "version": (1, 1),
     'location': 'View 3D > Object',
     'category': '3D View',
     "author": "Starfelll",
@@ -12,10 +12,18 @@ bl_info = {
 }
 
 
+def _is_locked(custom_normal: bpy.types.Attribute):
+    if custom_normal != None:
+        if custom_normal.data_type == "FLOAT_VECTOR":
+            if custom_normal.domain == "CORNER":
+                return True
+    return False
+
+
 class OP_LockNormals(bpy.types.Operator):
     bl_idname = "starfelll.lock_normals"
-    bl_label = "锁定法线"
-    bl_description = "禁用网格物体的法线计算，可以提高网格物体的场景性能"
+    bl_label = "Lock Normals"
+    bl_description = "禁用网格物体的法线自动计算，可以提高网格物体的场景性能"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context: bpy.types.Context):
@@ -28,9 +36,8 @@ class OP_LockNormals(bpy.types.Operator):
             mesh_num += 1
 
             attribute = mesh.attributes.get("custom_normal")
-            if not attribute is None:
-                if attribute.data_type == "FLOAT_VECTOR":
-                    continue
+            if _is_locked(attribute):
+                continue
             
             split_normals = []
             for poly in mesh.polygons:
@@ -38,8 +45,8 @@ class OP_LockNormals(bpy.types.Operator):
                     normal = mesh.loops[loop_index].normal.copy()
                     split_normals.append(normal)
             
-            if not attribute is None:
-                attribute.name = "custom_normal_backup"
+            if attribute != None:
+                mesh.attributes.remove(attribute)
 
             attribute = mesh.attributes.new(
                 name="custom_normal", 
@@ -57,7 +64,7 @@ class OP_LockNormals(bpy.types.Operator):
 
 class OP_UnlockNormals(bpy.types.Operator):
     bl_idname = "starfelll.unlock_normals"
-    bl_label = "解锁法线"
+    bl_label = "Unlock Normals"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context: bpy.types.Context):
@@ -69,44 +76,41 @@ class OP_UnlockNormals(bpy.types.Operator):
             mesh: bpy.types.Mesh = obj.data
             mesh_num += 1
             
-            custom_normal_backup = mesh.attributes.get("custom_normal_backup")
             custom_normal = mesh.attributes.get("custom_normal")
 
-            if custom_normal and custom_normal.data_type == "FLOAT_VECTOR":
-                if custom_normal_backup:
-                    mesh.attributes.remove(custom_normal)
-                    custom_normal_backup.name = "custom_normal"
-                    custom_normal = custom_normal_backup
-                    custom_normal_backup = None
-                else:
-                    split_normals = []
-                    for data in custom_normal.data:
-                        normal = data.vector
-                        split_normals.append(Vector([normal[0], normal[1], normal[2]]))
-                    mesh.attributes.remove(custom_normal)
-                    mesh.normals_split_custom_set(split_normals)
-                report_num += 1
+            if not _is_locked(custom_normal):
+                continue
 
-            if custom_normal_backup:
-                mesh.attributes.remove(custom_normal_backup)
+            split_normals = []
+            for data in custom_normal.data:
+                normal = data.vector
+                split_normals.append(Vector([normal[0], normal[1], normal[2]]))
+            mesh.attributes.remove(custom_normal)
+            mesh.normals_split_custom_set(split_normals)
+            report_num += 1
 
         self.report({'INFO'}, f"已解锁{report_num}/{mesh_num}个网格的法线")
         return {'FINISHED'}
 
 
-def draw_menu(this: bpy.types.Menu, _):
-    this.layout.operator(OP_LockNormals.bl_idname, icon="LOCKED")
-    this.layout.operator(OP_UnlockNormals.bl_idname, icon="UNLOCKED")
-    
-
+def draw_menu(this: bpy.types.Menu, context: bpy.types.Context):
+    if context.active_object.type != "MESH":
+        return
+    this.layout.separator()
+    mesh: bpy.types.Mesh = context.active_object.data
+    if _is_locked(mesh.attributes.get("custom_normal")):
+        this.layout.operator(OP_UnlockNormals.bl_idname, icon="UNLOCKED")
+    else:
+        this.layout.operator(OP_LockNormals.bl_idname, icon="LOCKED")
 
 def register():
    bpy.utils.register_class(OP_LockNormals)
    bpy.utils.register_class(OP_UnlockNormals)
-   bpy.types.VIEW3D_MT_object.append(draw_menu)
+   bpy.types.VIEW3D_MT_object_context_menu.append(draw_menu)
+
 
 def unregister():
-    bpy.types.VIEW3D_MT_object.remove(draw_menu)
+    bpy.types.VIEW3D_MT_object_context_menu.remove(draw_menu)
     bpy.utils.unregister_class(OP_LockNormals)
     bpy.utils.unregister_class(OP_UnlockNormals)
 
